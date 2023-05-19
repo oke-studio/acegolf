b28f68337463bfda8b59d82d20b9d52029ee9d2b
efe osah <onanefeosah@icloud.com>
2023-05-19 03:36:51 -0400

    install nextjs and material UI

---
+++ src/app/comingsoon/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import * as React from "react";
+import { Button } from "@mui/material";
+
+export default function Page() {
+    return <Button variant="contained">im here</Button>;
+}
