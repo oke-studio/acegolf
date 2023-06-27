e905efd50d807f6cdd581a6c1617b9fa0598d0d8
efe osah <onanefeosah@icloud.com>
2023-06-27 00:04:56 -0400

    pushing wip changes so coming soon page routing is fixed

--- src/app/comingsoon/page.tsx
+++ src/app/comingsoon/page.tsx
@@ -7,14 +7,27 @@ import {
     FormControl,
     InputLabel,
     OutlinedInput,
+    Grid,
+    Box,
+    Grow,
+    Collapse,
+    useMediaQuery,
 } from "@mui/material";
 import Image from "next/image";
 import { styled } from "@mui/system";
 
+import GolfLogo from "../../../public/images/ace-golf-golf-logo2.svg";
+import GolfLogoRight from "../../../public/images/ace-golf-golf-logo-right.svg";
+import GolfLogoLeft from "../../../public/images/ace-golf-golf-logo-left.svg";
+import GolfLogoCentre from "../../../public/images/ace-golf-golf-logo-centre.svg";
+
 const SidebarImages = [
     "/images/ace-golf-card.svg",
     "/images/ace-golf-bar-lounge.svg",
+    "/images/ace-golf-full.svg",
 ];
+
+const backgroundColor = "#529DC8";
 
 const ComingSoonInfoBox = styled("div")({
     display: "flex",
@@ -26,40 +39,299 @@ const ComingSoonInfoBox = styled("div")({
     gap: "16px",
 });
 
+const RelativePositionWrapper = styled("div")({
+    position: "relative",
+});
+
+const ComingSoonFormMobile = () => {};
+
 const ComingSoonForm = () => {
+    const [isSubmit, setIsSubmit] = React.useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitted");
+        setIsSubmit(true);
+    };
     return (
-        <ComingSoonInfoBox>
-            Coming soon...
-            <FormControl>
-                <InputLabel htmlFor="component-outlined">Email</InputLabel>
-                <OutlinedInput
-                    color="primary"
-                    id="component-outlined"
-                    defaultValue="Composed TextField"
-                    label="Email"
-                />
-            </FormControl>
-        </ComingSoonInfoBox>
+        <Box component="form" order={2}>
+            <TextField
+                fullWidth
+                id="email form"
+                type="email"
+                placeholder="yourname@email.com"
+                style={{ alignItems: "center" }}
+                helperText="*BY CLICKING THE BUTTON BELOW YOU ARE AGREEING ACEGOLF’S TERMS OF SERVICES AND TO RECEVING MARKETING EMAILS FROM ACEGOLF. 
+                YOU ARE ABLE TO UNSUBSCRIBE AT ANY TIME BY CLICKING UNSUBSCRBE FROM ANY FUTURE MARKETING EMAILS. "
+                sx={{
+                    fontFamily: "new-hero",
+                    ".MuiInputBase-root": {
+                        background: "white",
+                    },
+                    ".MuiInputBase-input": {
+                        color: "#373737",
+                        // background: "white",
+                        width: isSubmit ? "0" : "100%",
+                    },
+                    pointerEvents: isSubmit && "none",
+                }}
+                InputProps={{
+                    endAdornment: (
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            onClick={(e) => handleSubmit(e)}
+                            size="small"
+                            sx={{
+                                background: backgroundColor,
+                                // ":hover": {},
+                                width: isSubmit ? "100%" : "max-content",
+                                marginTop: "6px",
+                                marginBottom: "6px",
+                                whiteSpace: "nowrap",
+                                borderRadius: "12px",
+                                transition: "min-width 500ms ease-out",
+                                pointerEvents: isSubmit && "none",
+                                ":hover": {
+                                    backgroundColor: backgroundColor,
+                                },
+                                cursor: isSubmit && "default",
+                            }}
+                        >
+                            <span
+                                style={{
+                                    paddingLeft: "24px",
+                                    paddingRight: "24px",
+                                    paddingTop: "14px",
+                                    paddingBottom: "14px",
+                                    fontFamily: "new-hero",
+                                    fontWeight: "700",
+                                }}
+                            >
+                                {isSubmit
+                                    ? "WE'LL KEEP IN TOUCH! SEE YOU SOON!"
+                                    : "NOTIFY ME"}
+                            </span>
+                        </Button>
+                    ),
+                    sx: { borderRadius: "12px" },
+                }}
+            />
+        </Box>
     );
 };
 
-export default function Page() {
-    // const sidebarChoice = Math.floor(Math.random() * (1 - 0 + 1)) + 1;
+const ComingSoonInfoSectionWrapper = styled(Box)(({ theme }) => ({
+    marginTop: "60px",
+    marginBottom: "60px",
+    display: "grid",
+    // flexDirection: "row",
+    // alignItems: "center",
+    justifyContent: "center",
+    gridTemplateColumns: "1fr 2fr 1fr",
+    width: "100%",
+    fontFamily: "new-hero",
+}));
+
+const ComingSoonInfoWrapper = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+}));
+
+const ComingSoonInfoSection = () => (
+    <ComingSoonInfoSectionWrapper>
+        <ComingSoonInfoWrapper order={1}>
+            <img src="/images/ace-golf-logo.svg" alt="ace golf logo" />
+        </ComingSoonInfoWrapper>
+        <ComingSoonForm />
+        <ComingSoonInfoWrapper order={3}>
+            <span>FIND YOUR SWING</span>
+            <span>SUMMER 2023</span>
+        </ComingSoonInfoWrapper>
+    </ComingSoonInfoSectionWrapper>
+);
+
+const ComingSoonInfoSectionMobileWrapper = styled(Box)(({ theme }) => ({
+    marginTop: "60px",
+    marginBottom: "60px",
+    display: "flex",
+    flexDirection: "column",
+    // alignItems: "center",
+    justifyContent: "center",
+
+    width: "100%",
+    fontFamily: "new-hero",
+    gap: "24px",
+    marginLeft: "12px",
+    marginRight: "12px",
+}));
+
+const ComingSoonInfoMobileWrapper = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    // [theme.breakpoints.down(640)]: {
+    //     marginLeft: "12px",
+    //     marginRight: "12px",
+    // },
+    gap: "10px",
+    whiteSpace: "nowrap",
+}));
+
+const ComingSoonInfoSectionMobile = () => (
+    <ComingSoonInfoSectionMobileWrapper>
+        <ComingSoonInfoMobileWrapper order={1}>
+            <img src="/images/ace-golf-logo.svg" alt="ace golf logo" />
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "centre",
+                }}
+            >
+                <span>FIND YOUR SWING</span>
+                <span>SUMMER 2023</span>
+            </Box>
+        </ComingSoonInfoMobileWrapper>
+        <ComingSoonForm />
+    </ComingSoonInfoSectionMobileWrapper>
+);
+
+const LogoMap = () => {
+    let logoStack = [];
+
+    // Array.from(
+    //     Array(4).map((_, index) => {
+    //         console.log(index);
+    //         logoStack.push(<img src={GolfLogo} alt="temp" key={index} />);
+    //     })
+    // );
+
+    for (let index = 0; index < 4; index++) {
+        logoStack.push(
+            // <Image
+            //     src={golfLogo}
+            //     width={width}
+            //     height={height}
+            //     alt="temp alt"
+            //     key={index}
+            // />
+
+            <RelativePositionWrapper>
+                <GolfLogo key={index} position="absolute" height="100%" />
+            </RelativePositionWrapper>
+        );
+    }
+
+    // return (
+    //     <div
+    //         style={{
+    //             display: "flex",
+    //             // gridTemplateColumns: "repeat(4, 1fr)",
+    //             width: "100%",
+    //         }}
+    //     >
+    //         <RelativePositionWrapper>
+    //             <GolfLogoLeft
+    //                 position="absolute"
+    //                 height="100%"
+    //                 // viewBox={viewBox}
+    //             />
+    //         </RelativePositionWrapper>
+    //         <RelativePositionWrapper>
+    //             <GolfLogoCentre
+    //                 position="absolute"
+    //                 height="100%"
+    //                 // viewBox={viewBox}
+    //             />
+    //         </RelativePositionWrapper>
+    //         <RelativePositionWrapper>
+    //             <GolfLogoCentre
+    //                 position="absolute"
+    //                 height="100%"
+    //                 // viewBox={viewBox}
+    //             />
+    //         </RelativePositionWrapper>
+    //         <RelativePositionWrapper>
+    //             <GolfLogoRight
+    //                 position="absolute"
+    //                 height="100%"
+    //                 // viewBox={viewBox}
+    //             />
+    //         </RelativePositionWrapper>
+    //     </div>
+    // );
 
     return (
-        <div style={{ display: "flex", flexDirection: "row", height: "100vh" }}>
-            <div style={{ display: "flex", width: "35%" }}>
-                <img
-                    src={SidebarImages[0]}
-                    alt="ace golf card"
-                    style={{ maxWidth: "100%" }}
-                    // width={500}
-                    // height={500}
-                />
-            </div>
-            <div style={{ display: "flex", width: "65%" }}>
-                <ComingSoonForm />
-            </div>
+        <Box sx={{ display: "grid", gridTemplateRows: "repeat(4, 1fr)" }}>
+            {logoStack}
+        </Box>
+    );
+};
+
+const PageWrapper = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "100%",
+    // [theme.breakpoints.up('md')]: {
+
+    // }
+}));
+
+export default function Page() {
+    const isMobile = useMediaQuery("(max-width:640px)");
+
+    let topLogoVertStack = [];
+    let bottomLogoVertStack = [];
+
+    // Array.from(Array(4).map((_, index) => {}));
+
+    for (let index = 0; index < 4; index++) {
+        topLogoVertStack.push(<LogoMap />);
+    }
+
+    return (
+        <div
+            style={{
+                backgroundColor: backgroundColor,
+                height: "100vh",
+                color: "black",
+                width: "100vw",
+                overflow: "hidden",
+            }}
+        >
+            <PageWrapper>
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(4, 1fr)",
+                    }}
+                >
+                    Hi
+                </div>
+                {isMobile ? (
+                    <ComingSoonInfoSectionMobile />
+                ) : (
+                    <ComingSoonInfoSection />
+                )}
+
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        flexWrap: "nowrap",
+                        height: "100%",
+                        position: "relative",
+                        // top: "-10px",
+                    }}
+                >
+                    {topLogoVertStack}
+                </div>
+            </PageWrapper>
         </div>
     );
 }
