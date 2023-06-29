87f7da4edd2056d3a932dc21075db986a3fac92d
efe osah <onanefeosah@icloud.com>
2023-06-29 01:12:07 -0400

    add animations

--- src/app/comingsoon/components/comingSoonInfoSection/comingSoonInfoSection.component.tsx
+++ src/app/comingsoon/components/comingSoonInfoSection/comingSoonInfoSection.component.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import Image from 'next/image';
 import { styled, Box, TextField, Button, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
 
 const backgroundColor = '#529DC8';
 
@@ -42,40 +43,67 @@ const ComingSoonForm = () => {
         }}
         InputProps={{
           endAdornment: (
-            <Button
-              variant="contained"
-              type="submit"
+            // <Button
+            //   variant="contained"
+            //   type="submit"
+            //   onClick={(e) => handleSubmit(e)}
+            //   size="small"
+            //   sx={{
+            //     background: backgroundColor,
+            //     // ":hover": {},
+            //     width: isSubmit ? '100%' : 'max-content',
+            //     marginTop: '14px',
+            //     marginBottom: '14px',
+            //     whiteSpace: 'nowrap',
+            //     borderRadius: '12px',
+            //     transition: 'min-width 500ms ease-out',
+            //     pointerEvents: isSubmit ? 'none' : 'initial',
+            //     ':hover': {
+            //       backgroundColor: backgroundColor,
+            //     },
+            //     cursor: isSubmit ? 'default' : 'pointer',
+            //   }}
+            // >
+            //   <span
+            //     style={{
+            //       paddingLeft: '24px',
+            //       paddingRight: '24px',
+            //       paddingTop: '14px',
+            //       paddingBottom: '14px',
+            //       fontFamily: 'new-hero',
+            //       fontWeight: '700',
+            //     }}
+            //   >
+            //     {isSubmit ? "WE'LL KEEP IN TOUCH! SEE YOU SOON!" : 'NOTIFY ME'}
+            //   </span>
+            // </Button>
+
+            <motion.button
+              animate={isSubmit && { width: '100%' }}
+              initial={{ width: 'max-content' }}
+              transition={{ duration: 4, ease: 'easeInOut' }}
               onClick={(e) => handleSubmit(e)}
-              size="small"
-              sx={{
+              style={{
                 background: backgroundColor,
-                // ":hover": {},
-                width: isSubmit ? '100%' : 'max-content',
+                border: 'none',
+                borderRadius: '12px',
+                whiteSpace: 'nowrap',
                 marginTop: '14px',
                 marginBottom: '14px',
-                whiteSpace: 'nowrap',
-                borderRadius: '12px',
-                transition: 'min-width 500ms ease-out',
-                pointerEvents: isSubmit ? 'none' : 'initial',
-                ':hover': {
-                  backgroundColor: backgroundColor,
-                },
-                cursor: isSubmit ? 'default' : 'pointer',
               }}
             >
-              <span
-                style={{
-                  paddingLeft: '24px',
-                  paddingRight: '24px',
-                  paddingTop: '14px',
-                  paddingBottom: '14px',
-                  fontFamily: 'new-hero',
-                  fontWeight: '700',
-                }}
+              <Typography
+                paddingLeft="24px"
+                paddingRight="24px"
+                paddingTop="14px"
+                paddingBottom="14px"
+                fontFamily="new-hero"
+                fontWeight="700"
+                whiteSpace="no-wrap"
               >
                 {isSubmit ? "WE'LL KEEP IN TOUCH! SEE YOU SOON!" : 'NOTIFY ME'}
-              </span>
-            </Button>
+              </Typography>
+            </motion.button>
           ),
           sx: { borderRadius: '12px' },
         }}
