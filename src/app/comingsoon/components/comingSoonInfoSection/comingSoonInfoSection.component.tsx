8822f7d27b0c49b153c4208aa567b44d468d55b5
efe osah <onanefeosah@icloud.com>
2023-06-29 01:20:37 -0400

    fix ts errors

--- src/app/comingsoon/components/comingSoonInfoSection/comingSoonInfoSection.component.tsx
+++ src/app/comingsoon/components/comingSoonInfoSection/comingSoonInfoSection.component.tsx
@@ -44,41 +44,6 @@ const ComingSoonForm = () => {
         }}
         InputProps={{
           endAdornment: (
-            // <Button
-            //   variant="contained"
-            //   type="submit"
-            //   onClick={(e) => handleSubmit(e)}
-            //   size="small"
-            //   sx={{
-            //     background: backgroundColor,
-            //     // ":hover": {},
-            //     width: isSubmit ? '100%' : 'max-content',
-            //     marginTop: '14px',
-            //     marginBottom: '14px',
-            //     whiteSpace: 'nowrap',
-            //     borderRadius: '12px',
-            //     transition: 'min-width 500ms ease-out',
-            //     pointerEvents: isSubmit ? 'none' : 'initial',
-            //     ':hover': {
-            //       backgroundColor: backgroundColor,
-            //     },
-            //     cursor: isSubmit ? 'default' : 'pointer',
-            //   }}
-            // >
-            //   <span
-            //     style={{
-            //       paddingLeft: '24px',
-            //       paddingRight: '24px',
-            //       paddingTop: '14px',
-            //       paddingBottom: '14px',
-            //       fontFamily: 'new-hero',
-            //       fontWeight: '700',
-            //     }}
-            //   >
-            //     {isSubmit ? "WE'LL KEEP IN TOUCH! SEE YOU SOON!" : 'NOTIFY ME'}
-            //   </span>
-            // </Button>
-
             <motion.button
               animate={isSubmit && { width: '100%' }}
               initial={{ width: 'max-content' }}
