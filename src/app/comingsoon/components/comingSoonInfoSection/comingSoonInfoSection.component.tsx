5be39345e94c2354a67885b125a7ad3a29e1014d
efe osah <onanefeosah@icloud.com>
2023-06-30 13:48:39 -0400

    form fixes_3

--- src/app/comingsoon/components/comingSoonInfoSection/comingSoonInfoSection.component.tsx
+++ src/app/comingsoon/components/comingSoonInfoSection/comingSoonInfoSection.component.tsx
@@ -12,7 +12,7 @@ interface FormValues {
   email: string;
 }
 
-const encode = (data: FormValues) => {
+const encode = (data) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
