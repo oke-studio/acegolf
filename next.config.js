777a6a88803998c6827c1f8cd2546f4a1245f9b8
efe osah <onanefeosah@icloud.com>
2023-07-11 22:23:42 -0400

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/comingsoon',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
