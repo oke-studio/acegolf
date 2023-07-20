286189f2dc2a07fa47ebf2ee9f33320b2ac5e937
efe osah <onanefeosah@icloud.com>
2023-07-20 08:47:18 -0400

    add mobile top banner

--- src/app/comingsoon/comingSoonPage.component.tsx
+++ src/app/comingsoon/comingSoonPage.component.tsx
@@ -46,17 +46,21 @@ export default function ComingSoonPage() {
               flexDirection: 'row',
               overflow: 'hidden',
               position: 'relative',
+              width: '100%',
+              top: '35px',
+              transform: 'scale(1.6)',
             }}
           >
-            {/* <TopBanner
-              width="1973"
-              height="293"
+            <TopBanner
+              width="100%"
+              height="100%"
+              viewBox="0 0 1973 293"
               pathProps={{
                 d: 'M 0.3257 244.537 C 21 244.537 28.5 238.5 142.377 169.919 C 343 49.0589 549.615 31.5 474.992 123.111 C 380.659 238.919 596.811 193.13 832.992 123.11 C 1255 -1.9998 1630.81 38.1808 1413.49 179.111 C 1248.49 286.111 1913 102.502 1972 102.502',
               }}
               text="TORONTO'S FIRST EVER INDOOR GOLF LOUNGE"
               textCount={4}
-            /> */}
+            />
           </div>
           <ComingSoonInfoSectionMobile />
           <div
