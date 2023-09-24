b93722edcc1e05cf129112c3348793566462df11
Efe Osah <57697426+efeosah@users.noreply.github.com>
2023-09-24 19:55:34 -0400

    33 home page hero section desktop mobile (#34)
    * first commit
    
    * nit changes

---
+++ src/app/landing/components/backdrop/textBackdrop.component.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { SxProps, Box, Theme, CSSObject } from '@mui/material';
+
+export const TextWithBackdrop = ({
+  backdropColor,
+  contentColor,
+  height,
+  xOffset,
+  yOffset,
+  width,
+  text,
+  children,
+  css,
+  afterProps,
+}: {
+  text: string;
+  backdropColor: string;
+  contentColor: string;
+  height: string | number;
+  width: string | number;
+  xOffset: number;
+  yOffset: number;
+  children?: React.ReactNode;
+  css?: SxProps;
+  afterProps?: CSSObject;
+}) => {
+  return (
+    <Box
+      sx={{
+        ...css,
+        width: width,
+        height: height,
+        backgroundColor: backdropColor,
+        position: 'relative',
+        '::after': {
+          ...afterProps,
+          content: `"${text}"`,
+          width: '100%',
+          height: '100%',
+          display: 'inline-flex',
+          top: `${yOffset}px`,
+          left: `${xOffset}px`,
+          position: 'absolute',
+          // background: isImage ? `url(${img})` : 'blue',
+          background: contentColor,
+          borderRadius: 'inherit',
+        },
+      }}
+    >
+      {/* <Box
+          sx={{
+            backgroundColor: 'orange',
+            position: 'relative',
+            height: 'inherit',
+            width: 'inherit',
+            top: '10px',
+            left: '15px',
+          }}
+        > */}
+      {children}
+      {/* </Box> */}
+    </Box>
+  );
+};
