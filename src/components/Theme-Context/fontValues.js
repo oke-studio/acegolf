cf08b5d510cdbda4100b9277e4b9eff686b7858c
Seyitan Oke <seyitanoke@gmail.com>
2023-12-18 10:08:33 -0500

    Added a rough  file with the theme values that will be used

---
+++ src/components/Theme-Context/fontValues.js
@@ -0,0 +1,149 @@
+typeScale:[
+    {
+        1:{
+        fontSize: "12px",
+        lineHeight: "17px" 
+        } 
+    },
+    {
+        2:{
+        fontSize: "14px",
+        lineHeight: "20px" 
+        } 
+    },
+    {
+        3:{
+        fontSize: "16px",
+        lineHeight: "22px" 
+        } 
+    },
+    {
+        4:{
+        fontSize: "18px",
+        lineHeight: "24px" 
+        } 
+    },
+    {
+        5:{
+        fontSize: "20px",
+        lineHeight: "28px" 
+        } 
+    },
+    {
+        6:{
+        fontSize: "24px",
+        lineHeight: "32px" 
+        } 
+    },
+    {
+        7:{
+        fontSize: "30px",
+        lineHeight: "36px" 
+        } 
+    },
+    {
+        8:{
+        fontSize: "36px",
+        lineHeight: "42px" 
+        } 
+    },
+    {
+        9:{
+        fontSize: "56px",
+        lineHeight: "60px" 
+        } 
+    },
+    {
+        10:{
+        fontSize: "72px",
+        lineHeight: "72px" 
+        } 
+    },
+    {
+        11:{
+        fontSize: "96px",
+        lineHeight: "96px" 
+        } 
+    }
+
+];
+
+
+
+
+fontUses:[
+    {
+        miniscule:{
+            fontSize: typeScale[1].fontSize,
+            lineHeight: typeScale[1].lineHeight
+        }
+    },
+
+    {
+        small:{
+         fontSize: typeScale[2].fontSize,
+         lineHeight: typeScale[2].lineHeight
+            }
+    },
+    {
+        base:{
+            fontSize: typeScale[3].fontSize,
+            lineHeight: typeScale[3].lineHeight
+        }
+    },
+
+    {
+        large:{
+         fontSize: typeScale[4].fontSize,
+         lineHeight: typeScale[4].lineHeight
+            }
+    },
+    {
+        extralarge:{
+            fontSize: typeScale[5].fontSize,
+            lineHeight: typeScale[5].lineHeight
+        }
+    },
+    {
+        h4:{
+         fontSize: typeScale[6].fontSize,
+         lineHeight: typeScale[6].lineHeight
+            }
+    },
+    {
+        h3:{
+            fontSize: typeScale[7].fontSize,
+            lineHeight: typeScale[7].lineHeight
+        }
+    },
+
+    {
+        h2:{
+         fontSize: typeScale[8].fontSize,
+         lineHeight: typeScale[8].lineHeight
+            }
+    },
+    {
+        h1:{
+            fontSize: typeScale[9].fontSize,
+            lineHeight: typeScale[9].lineHeight
+        }
+    },
+    {
+        largeH1:{
+         fontSize: typeScale[10].fontSize,
+         lineHeight: typeScale[10].lineHeight
+            }
+    },
+    {
+        poster:{
+            fontSize: typeScale[11].fontSize,
+            lineHeight: typeScale[11].lineHeight
+        }
+    }
+    
+]
+
+fontWeight:[ 200, 300, 400, 500, 600, 700, 800, 900];
+
+fontStyle:[normal, italic];
