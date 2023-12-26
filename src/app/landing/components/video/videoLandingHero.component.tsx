fbc7c48275d8872087b6ea3077cd9eb020be7296
Seyitan Oke <seyitanoke@gmail.com>
2023-12-26 01:54:19 -0500

    added video text path

--- src/app/landing/components/video/videoLandingHero.component.tsx
+++ src/app/landing/components/video/videoLandingHero.component.tsx
@@ -10,6 +10,7 @@ import {
 import Image from 'next/image';
 import { Section } from '@/components/layout/section.component';
 import { LandingVideo } from '../../components/video/video.component';
+import { RoundedVideoPath } from '@/components/animated-paths/roundedVideoPath.component';
 
 export const VideoLandingHero = () => {
   return (
@@ -20,23 +21,23 @@ export const VideoLandingHero = () => {
       CornerRadius="on"
       sx={{}}
     >
-        <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1152 627">
-  
-  <g id="Layer_1-2" data-name="Layer 1">
-    <rect 
-        style={{
-        
-            fill: "none",
-            stroke:" #171717",
-            strokeMiterlimit: "10"
-          
-        }}
-        x=".5" y=".5" width="1151" height="626" rx="35.79" ry="35.79"/>
-  </g>
-</svg>
 
-
+      <RoundedVideoPath
+      width="100%"
+      height="100%"
+      viewBox="0 0 1152 628"
+      pathProps={{
+        d: 'm1116.5,628H35.5c-19.57,0-35.5-15.92-35.5-35.5V35.5C0,15.93,15.93,0,35.5,0h1081c19.58,0,35.5,15.93,35.5,35.5v557c0,19.58-15.92,35.5-35.5,35.5ZM35.5,1C16.48,1,1,16.48,1,35.5v557c0,19.02,15.48,34.5,34.5,34.5h1081c19.02,0,34.5-15.48,34.5-34.5V35.5c0-19.02-15.48-34.5-34.5-34.5H35.5Z',
+      }}
+      text="WE BRING YOU GOLF, ALL-YEAR ROUND WITH STATE OF THE ART GOLF BAYS, THE BEST DRINKS IN THE CITY AND A CURATED SELECTION OF BITES TO FUEL YOUR SWING."
+      textCount={2}
+      strokeColor="#EB8B32"
+      
+      
+      ></RoundedVideoPath>
       <LandingVideo></LandingVideo>
+      
+      
     </Section>
   );
 };
