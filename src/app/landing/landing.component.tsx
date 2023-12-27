da6212d92035cdf321a7ed74931d6cc78ac25fa7
Seyitan Oke <seyitanoke@gmail.com>
2023-12-27 16:31:25 -0500

    fixing order of lannding how it works

--- src/app/landing/landing.component.tsx
+++ src/app/landing/landing.component.tsx
@@ -64,7 +64,7 @@ export default function Landing() {
       /> */}
       <LandingHero />
       <VideoLandingHero />
-      <LandingHowItWorks />
+
       {/* Banner Iterlude */}
       <Box
         sx={{
@@ -86,7 +86,7 @@ export default function Landing() {
             viewBox="0 0 1440 400"
             pathProps={{
               d: 'M 0.3257 244.537 C 21 244.537 28.5 238.5 142.377 169.919 C 343 49.0589 549.615 31.5 474.992 123.111 C 380.659 238.919 596.811 193.13 832.992 123.11 C 1255 -1.9998 1630.81 38.1808 1413.49 179.111 C 1248.49 286.111 1913 102.502 1924 222',
-              fill: '#EB8B32',
+              //fill: '#EB8B32',
             }}
             text=" "
             textCount={1}
@@ -99,7 +99,7 @@ export default function Landing() {
             height: '500px',
             width: '100vw',
             position: 'relative',
-            top: isMobile ? '-515px' : '-530px',
+           // top: isMobile ? '-515px' : '-530px',
             ...(isMobile && { scale: '2' }),
           }}
         >
@@ -112,10 +112,13 @@ export default function Landing() {
             }}
             text="FOOD  DRINKS FUN "
             textCount={5}
-            strokeColor="#171717"
+            strokeColor="#529DC8"
           />
         </Box>
       </Box>
+
+      <LandingHowItWorks />
+
       {/* Orange Section */}
       <Box
         sx={{
@@ -127,7 +130,7 @@ export default function Landing() {
           justifyContent: 'center',
           padding: '0px 48px',
           position: 'relative',
-          top: '-940px',
+          //top: '-940px',
           right: 0,
           left: 0,
           alignContent: 'center',
