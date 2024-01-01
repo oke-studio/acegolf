4057370aaeec984e7bd58482ee267c5a676ff1d1
Seyitan Oke <seyitanoke@gmail.com>
2024-01-01 17:54:18 -0500

    tweaking cards to match design

--- src/app/landing/landing.component.tsx
+++ src/app/landing/landing.component.tsx
@@ -22,6 +22,7 @@ import { MailingList } from './components/mailingList/mailingList.component';
 import { LandingPromotionMailingSection } from './components/promotionMailing/landingPromotionMailingSection.component';
 import { LandingInfoSection } from './components/landingInfo/landingInfoSection.component';
 import { NintendoSwitch } from '@/components/nintendoSwitch/nintendoSwitch.component';
+import { LandingCardsGrid } from './components/landingCards/landingCardsGrid.component';
 
 const MapSVG = () => (
   <svg
@@ -69,8 +70,9 @@ export default function Landing() {
       <LandingHowItWorks />
 
       <LandingInfoSection />
+
       <LandingPromotionMailingSection />
-      <NintendoSwitch/>
+      <NintendoSwitch />
     </Box>
   );
 }
