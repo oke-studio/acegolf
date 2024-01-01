549dbf40033c8d7be1d5f5e3b36f6fc9b427e0ce
efe osah <onanefeosah@icloud.com>
2024-01-01 12:56:20 -0500

    relocate font files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,92 +10,92 @@ import { FooterV2 } from '@/components/Footer/footerV2.component';
 const aceFont = localfont({
   src: [
     {
-      path: 'fonts/Hero New HairLine.woff',
+      path: '../../public/fonts/Hero New HairLine.woff',
       weight: '100',
     },
     {
-      path: 'fonts/Hero New HairLine Italic.woff',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: 'fonts/Hero New Thin.woff',
-      weight: '100',
-    },
-    {
-      path: 'fonts/Hero New Thin Italic.woff',
+      path: '../../public/fonts/Hero New HairLine Italic.woff',
       weight: '100',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New UltraLight.woff',
+      path: '../../public/fonts/Hero New Thin.woff',
+      weight: '100',
+    },
+    {
+      path: '../../public/fonts/Hero New Thin Italic.woff',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Hero New UltraLight.woff',
       weight: '200',
     },
     {
-      path: 'fonts/Hero New UltraLight Italic.woff',
+      path: '../../public/fonts/Hero New UltraLight Italic.woff',
       weight: '200',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New Light.woff',
+      path: '../../public/fonts/Hero New Light.woff',
       weight: '300',
     },
     {
-      path: 'fonts/Hero New Light Italic.woff',
+      path: '../../public/fonts/Hero New Light Italic.woff',
       weight: '300',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New Regular.woff',
+      path: '../../public/fonts/Hero New Regular.woff',
       weight: '400',
     },
     {
-      path: 'fonts/Hero New Regular Italic.woff',
+      path: '../../public/fonts/Hero New Regular Italic.woff',
       weight: '400',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New Medium.woff',
+      path: '../../public/fonts/Hero New Medium.woff',
       weight: '500',
     },
     {
-      path: 'fonts/Hero New Medium Italic.woff',
+      path: '../../public/fonts/Hero New Medium Italic.woff',
       weight: '500',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New SemiBold.woff',
+      path: '../../public/fonts/Hero New SemiBold.woff',
       weight: '600',
     },
     {
-      path: 'fonts/Hero New SemiBold Italic.woff',
+      path: '../../public/fonts/Hero New SemiBold Italic.woff',
       weight: '600',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New Bold.woff',
+      path: '../../public/fonts/Hero New Bold.woff',
       weight: '700',
     },
     {
-      path: 'fonts/Hero New Bold Italic.woff',
+      path: '../../public/fonts/Hero New Bold Italic.woff',
       weight: '700',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New ExtraBold.woff',
+      path: '../../public/fonts/Hero New ExtraBold.woff',
       weight: '800',
     },
     {
-      path: 'fonts/Hero New ExtraBold Italic.woff',
+      path: '../../public/fonts/Hero New ExtraBold Italic.woff',
       weight: '800',
       style: 'italic',
     },
     {
-      path: 'fonts/Hero New Super.woff',
+      path: '../../public/fonts/Hero New Super.woff',
       weight: '900',
     },
     {
-      path: 'fonts/Hero New Super Italic.woff',
+      path: '../../public/fonts/Hero New Super Italic.woff',
       weight: '900',
       style: 'italic',
     },
