cb803266046cfe98cbfe25190cf832768a5bb985
Seyitan Oke <seyitanoke@gmail.com>
2024-01-04 14:42:53 -0500

    more lint issues

--- src/app/landing/components/whatsTheVibe/whatsTheVibe.component.tsx
+++ src/app/landing/components/whatsTheVibe/whatsTheVibe.component.tsx
@@ -12,105 +12,106 @@ export const WhatsTheVibe = () => {
       SectionName="Whats the Vibe"
       SectionWidth="fullViewport"
       SectionColor=""
-      CornerRadius="off"
+      CornerRadius={false}
+      ScrollAnimations={false}
       sx={{
         display: 'flex',
         flexDirection: 'column',
       }}
     >
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '24px',
-        backgroundColor: '#171717',
-      }}
-    >
-      <Typography
-        variant="largeH1"
+      <Box
         sx={{
-          textTransform: 'uppercase',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '24px',
+          backgroundColor: '#171717',
         }}
       >
-        What&apos;s the Vibe?
-      </Typography>
-      <MusicPlayer />
-      {/* Picture collage */}
-      <Box sx={{ display: 'flex', margin: '75px 0px', gap: '24px' }}>
-        <Box
-          // src="/images/whatsTheVibeAssets/wtv-04.jpg"
-
+        <Typography
+          variant="largeH1"
           sx={{
-            backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            borderRadius: '8px',
-            width: '200px',
-            height: '300px',
-
-            transform: 'translateY(-25px)',
+            textTransform: 'uppercase',
           }}
-        />
-        <Box
-          // src="/images/whatsTheVibeAssets/wtv-04.jpg"
+        >
+          What&apos;s the Vibe?
+        </Typography>
+        <MusicPlayer />
+        {/* Picture collage */}
+        <Box sx={{ display: 'flex', margin: '75px 0px', gap: '24px' }}>
+          <Box
+            // src="/images/whatsTheVibeAssets/wtv-04.jpg"
 
+            sx={{
+              backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              borderRadius: '8px',
+              width: '200px',
+              height: '300px',
+
+              transform: 'translateY(-25px)',
+            }}
+          />
+          <Box
+            // src="/images/whatsTheVibeAssets/wtv-04.jpg"
+
+            sx={{
+              backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              borderRadius: '8px',
+              width: '200px',
+              height: '300px',
+
+              transform: 'translateY(50px)',
+            }}
+          />
+          <Box
+            // src="/images/whatsTheVibeAssets/wtv-04.jpg"
+
+            sx={{
+              backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              borderRadius: '8px',
+              width: '200px',
+              height: '300px',
+
+              transform: 'translateY(-25px)',
+              marginLeft: '24px',
+            }}
+          />
+          <Box
+            // src="/images/whatsTheVibeAssets/wtv-04.jpg"
+
+            sx={{
+              backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              borderRadius: '8px',
+              width: '200px',
+              height: '300px',
+
+              transform: 'translateY(50px)',
+            }}
+          />
+        </Box>
+
+        <Typography
+          variant="largeH1"
           sx={{
-            backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            borderRadius: '8px',
-            width: '200px',
-            height: '300px',
-
-            transform: 'translateY(50px)',
+            textTransform: 'uppercase',
           }}
-        />
-        <Box
-          // src="/images/whatsTheVibeAssets/wtv-04.jpg"
-
-          sx={{
-            backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            borderRadius: '8px',
-            width: '200px',
-            height: '300px',
-
-            transform: 'translateY(-25px)',
-            marginLeft: '24px',
-          }}
-        />
-        <Box
-          // src="/images/whatsTheVibeAssets/wtv-04.jpg"
-
-          sx={{
-            backgroundImage: 'url(/images/ace-banner-chromatic-white.jpg)',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            borderRadius: '8px',
-            width: '200px',
-            height: '300px',
-
-            transform: 'translateY(50px)',
-          }}
-        />
+        >
+          What they&apos;re saying?
+        </Typography>
       </Box>
-
-      <Typography
-        variant="largeH1"
-        sx={{
-          textTransform: 'uppercase',
-        }}
-      >
-        What they&apos;re saying?
-      </Typography>
-    </Box>
     </Section>
   );
 };
