2a096bf67d87712fb90191d2305d5d200fffe604
Seyitan Oke <seyitanoke@gmail.com>
2024-01-05 16:56:04 -0500

    lincked spotify through iframe

--- src/app/landing/components/whatsTheVibe/components/musicPlayer/musicPlayer.component.tsx
+++ src/app/landing/components/whatsTheVibe/components/musicPlayer/musicPlayer.component.tsx
@@ -2,6 +2,7 @@
 
 import { Box } from '@mui/material';
 import * as React from 'react';
+import Iframe from 'react-iframe';
 
 export const MusicPlayer = () => {
   return (
@@ -13,6 +14,19 @@ export const MusicPlayer = () => {
         display: 'flex',
         borderRadius: '8px',
       }}
-    ></Box>
+    >
+      <Iframe
+        url=""
+        styles={{ borderRadius: '12px' }}
+        src="https://open.spotify.com/embed/playlist/72hITZ4d6zKMgsczBtqREI?utm_source=generator"
+        width="100%"
+        height="152"
+        frameBorder={0}
+        allowFullScreen={false}
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        id="spotifyIframe"
+      />
+    </Box>
   );
 };
