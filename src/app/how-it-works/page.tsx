35a106b4ac44a2e781237ea482248a3c120c3a51
Seyitan Oke <seyitanoke@gmail.com>
2024-01-06 10:57:12 -0500

    how it works start

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -1,5 +1,19 @@
+'use client';
 import * as React from 'react';
+import { useEffect, useRef } from 'react';
+import { Box, useTheme, useMediaQuery } from '@mui/material';
+
+// Animation dependencies
+import { motion, useScroll, useTransform } from 'framer-motion';
+import Lenis from '@studio-freight/lenis';
+import { NintendoSwitch } from '@/components/nintendoSwitch/nintendoSwitch.component';
+import FullHowItWorks from './components/fullHowItWorks.component';
 
 export default function HowItWorks() {
-  return <div>How it works page</div>;
+  return (
+    <Box>
+      <FullHowItWorks />
+      <NintendoSwitch />
+    </Box>
+  );
 }
