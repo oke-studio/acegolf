b8efdd244e96f7f723b8924d7b0c3d9b5453d8bf
Seyitan Oke <seyitanoke@gmail.com>
2024-01-08 13:18:24 -0500

    added cancelation policy page

---
+++ src/app/cancellationPolicy/layout.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Header } from '@/components/Header/header.component';
+import { Footer } from '@/components/Footer/footer.component';
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ src/app/cancellationPolicy/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import * as React from 'react';
+import PolicyPage from './cancellationPolicy.component';
+
+export default function Policy() {
+  return (
+    <main style={{ height: '100vh', backgroundColor: '#171717' }}>
+      <PolicyPage />
+    </main>
+  );
+}
