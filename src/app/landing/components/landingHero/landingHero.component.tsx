751a39a19c5adcce2752ba0cc9da5a2dc1e2de19
Seyitan Oke <seyitanoke@gmail.com>
2024-01-24 16:16:46 -0500

    removed issue

--- src/app/landing/components/landingHero/landingHero.component.tsx
+++ src/app/landing/components/landingHero/landingHero.component.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import {
-	Button,
-	Box,
-	useMediaQuery,
-	// useTheme
-} from '@mui/material';
+import { Button, Box, useMediaQuery, useTheme } from '@mui/material';
 import Image from 'next/image';
 import { Section } from '@/components/layout/section.component';
 import { Typography } from '@/components/Typography/typography.component';
@@ -15,9 +10,8 @@ import { landingHeroImages } from './landingHeroImages';
 
 export const LandingHero = () => {
 	const router = useRouter();
-	const theme = useTheme();
-	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-	const side = false;
+	//const theme = useTheme();
+	//const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
 	return (
 		<Section
