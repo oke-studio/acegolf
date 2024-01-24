4c7ccd9dc729ca534b837b785eff678ee46348bc
Seyitan Oke <seyitanoke@gmail.com>
2024-01-24 11:37:17 -0500

    refactored the images into a seperate file

--- src/app/landing/components/landingHero/landingHero.component.tsx
+++ src/app/landing/components/landingHero/landingHero.component.tsx
@@ -4,12 +4,9 @@ import Image from 'next/image';
 import { Section } from '@/components/layout/section.component';
 import { Typography } from '@/components/Typography/typography.component';
 import { useRouter } from 'next/navigation';
-import Picture1 from '../../../../../public/images/Spin-photo1.png';
-import Picture2 from '../../../../../public/images/Spin-photo2.png';
-import Picture3 from '../../../../../public/images/Spin-photo3.png';
-import Picture4 from '../../../../../public/images/Spin-photo4.png';
 import { AceImage } from '@/components/aceImage/aceImage.component';
 import { SectionImageGrid } from '@/components/ImageLayoutGrids/sectionImageGrid.component';
+import { landingHeroImages } from './landingHeroImages';
 
 export const LandingHero = () => {
 	const router = useRouter();
@@ -17,112 +14,6 @@ export const LandingHero = () => {
 	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 	const side = false;
 
-	const dataImages = [
-		{
-			src: '/images/Spin-photo4.png',
-			aspectRatio: '1.136',
-			backCutOut: true,
-			backCutOutColor: 'orange',
-			imageNameAltText: 'the vibes at ace picture',
-			opacity: 1, // default 1 for visible
-			scale: 1.9, // scale transform for aceImage
-			rotation: -10, // rotation
-			zIndex: 1, // zindex for image container. 1= below section content
-			gridPosXColumn: '2', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-			gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-			relPosX: '-100px', //relative position to grid position
-			relPosY: '68px',
-
-			mobile: {
-				opacity: 1,
-				scale: 1.2,
-				rotation: -10,
-				zIndex: 3,
-				gridPosXColumn: '2',
-				gridPosYRow: '2',
-				relPosX: '-80px',
-				relPosY: '38px',
-			},
-		},
-		{
-			src: '/images/Spin-photo2.png',
-			aspectRatio: '0.710',
-			backCutOut: true,
-			backCutOutColor: 'sharpTeal',
-			imageNameAltText: 'the vibes at the bar',
-			opacity: 1,
-			scale: 1, // scale transform for aceImage
-			rotation: 1, // rotation
-			zIndex: 1, // zindex for image container
-			gridPosXColumn: '3', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-			gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-			relPosX: '0px', //relative position to grid position
-			relPosY: '0px',
-
-			mobile: {
-				opacity: 1,
-				scale: 1.2,
-				rotation: 10,
-				zIndex: 1,
-				gridPosXColumn: '2',
-				gridPosYRow: '3',
-				relPosX: '0px',
-				relPosY: '0px',
-			},
-		},
-		{
-			src: '/images/Spin-photo3.png',
-			aspectRatio: '1/1',
-			backCutOut: true,
-			backCutOutColor: 'sharpTeal',
-			imageNameAltText: 'the vibes at the bar',
-			opacity: 1,
-			scale: 1, // scale transform for aceImage
-			rotation: 1, // rotation
-			zIndex: 1, // zindex for image container
-			gridPosXColumn: '4', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-			gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-			relPosX: '0px', //relative position to grid position
-			relPosY: '0px',
-
-			mobile: {
-				opacity: 1,
-				scale: 1,
-				rotation: 10,
-				zIndex: 1,
-				gridPosXColumn: '4',
-				gridPosYRow: '2',
-				relPosX: '0px',
-				relPosY: '0px',
-			},
-		},
-		{
-			src: '/images/Spin-photo1.png',
-			aspectRatio: '0.637',
-			backCutOut: true,
-			backCutOutColor: 'sharpTeal',
-			imageNameAltText: 'the vibes at the bar',
-			opacity: 1,
-			scale: 1, // scale transform for aceImage
-			rotation: 1, // rotation
-			zIndex: 1, // zindex for image container
-			gridPosXColumn: '4', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-			gridPosYRow: '1', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-			relPosX: '0px', //relative position to grid position
-			relPosY: '0px',
-
-			mobile: {
-				opacity: 1,
-				scale: 1,
-				rotation: 10,
-				zIndex: 1,
-				gridPosXColumn: '4',
-				gridPosYRow: '1',
-				relPosX: '0px',
-				relPosY: '0px',
-			},
-		},
-	];
 	return (
 		<Section
 			SectionName="Landing Hero"
@@ -290,7 +181,7 @@ export const LandingHero = () => {
 			{/* Sectional Photos */}
 			<SectionImageGrid
 				SectionImageGridWidth="rightHalf"
-				ImageData={dataImages}
+				ImageData={landingHeroImages}
 			></SectionImageGrid>
 		</Section>
 	);
