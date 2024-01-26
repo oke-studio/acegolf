9605a3455ae054ed9e2fd5931af67b51653295af
Seyitan Oke <seyitanoke@gmail.com>
2024-01-25 21:31:31 -0500

    commit

--- src/app/landing/components/landingHero/landingHeroImages 2.ts
+++ src/app/landing/components/landingHero/landingHeroImages 2.ts
@@ -1,144 +1,143 @@
 export const landingHeroImages = [
-    {
-        src: '/images/friends-photos.jpg',
-        aspectRatio: '0.77',
-        backCutOut: true,
-        backCutOutColor: 'orange',
-        imageNameAltText: 'Gif of photos at ace',
-        opacity: 1, // default 1 for visible
-        scale: 1.4, // scale transform for aceImage
-        rotation: -5, // rotation
-        zIndex: 2, // zindex for image container. 1= below section content
-        gridPosXColumn: '2', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-        gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-        relPosX: '-80px', //relative position to grid position
-        relPosY: '70px',
-        paralax: 1,
-	  
+	{
+		src: '/images/friends-photos.jpg',
+		aspectRatio: '0.77',
+		backCutOut: true,
+		backCutOutColor: 'orange',
+		imageNameAltText: 'Gif of photos at ace',
+		opacity: 1, // default 1 for visible
+		scale: 1.4, // scale transform for aceImage
+		rotation: -5, // rotation
+		zIndex: 2, // zindex for image container. 1= below section content
+		gridPosXColumn: '2', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
+		gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
+		relPosX: '-80px', //relative position to grid position
+		relPosY: '70px',
+		paralax: 1,
 
-        mobile: {
-            aspectRatio: '1/1',
-            opacity: 1,
-            scale: 1.7,
-            rotation: -4,
-            zIndex: 1,
-            gridPosXColumn: '2',
-            gridPosYRow: '2',
-            relPosX: '-30px',
-            relPosY: '60px',
-        },
-    },
-    {
-        src: '/images/Spin-photo3.png',
-        aspectRatio: '1.130',
-        backCutOut: true,
-        backCutOutColor: 'sharpTeal',
-        imageNameAltText: 'the vibes seating area',
-        opacity: 1,
-        scale: 1.2, // scale transform for aceImage
-        rotation: -11, // rotation
-        zIndex: 1, // zindex for image container
-        gridPosXColumn: '3', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-        gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-        relPosX: '-80px', //relative position to grid position
-        relPosY: '30px',
-        paralax: 2,
+		mobile: {
+			aspectRatio: '1/1',
+			opacity: 1,
+			scale: 1.7,
+			rotation: -4,
+			zIndex: 1,
+			gridPosXColumn: '2',
+			gridPosYRow: '2',
+			relPosX: '-30px',
+			relPosY: '60px',
+		},
+	},
+	{
+		src: '/images/Spin-photo3.png',
+		aspectRatio: '1.130',
+		backCutOut: true,
+		backCutOutColor: 'sharpTeal',
+		imageNameAltText: 'the vibes seating area',
+		opacity: 1,
+		scale: 1.2, // scale transform for aceImage
+		rotation: -11, // rotation
+		zIndex: 1, // zindex for image container
+		gridPosXColumn: '3', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
+		gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
+		relPosX: '-80px', //relative position to grid position
+		relPosY: '30px',
+		paralax: 2,
 
-        mobile: {
-            aspectRatio: '1.130',
-            opacity: 1,
-            scale: 1.4,
-            rotation: -10,
-            zIndex: 1,
-            gridPosXColumn: '2',
-            gridPosYRow: '3',
-            relPosX: '-40px',
-            relPosY: '50px',
-        },
-    },
-    {
-        src: '/images/Spin-photo1.png',
-        aspectRatio: '0.95',
-        backCutOut: true,
-        backCutOutColor: 'sharpTeal',
-        imageNameAltText: 'pool bar',
-        opacity: 1,
-        scale: 1.4, // scale transform for aceImage
-        rotation: 8, // rotation
-        zIndex: 1, // zindex for image container
-        gridPosXColumn: '4', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-        gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-        relPosX: '-20px', //relative position to grid position
-        relPosY: '80px',
-        paralax: 1,
+		mobile: {
+			aspectRatio: '1.130',
+			opacity: 1,
+			scale: 1.4,
+			rotation: -10,
+			zIndex: 1,
+			gridPosXColumn: '2',
+			gridPosYRow: '3',
+			relPosX: '-40px',
+			relPosY: '50px',
+		},
+	},
+	{
+		src: '/images/Spin-photo1.png',
+		aspectRatio: '0.95',
+		backCutOut: true,
+		backCutOutColor: 'sharpTeal',
+		imageNameAltText: 'pool bar',
+		opacity: 1,
+		scale: 1.4, // scale transform for aceImage
+		rotation: 8, // rotation
+		zIndex: 1, // zindex for image container
+		gridPosXColumn: '4', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
+		gridPosYRow: '2', //enter the grid ROW start position. ex: 5 (start at grid line 5)
+		relPosX: '-20px', //relative position to grid position
+		relPosY: '80px',
+		paralax: 1,
 
-        mobile: {
-            aspectRatio: '0.95',
-            opacity: 1,
-            scale: 1.4,
-            rotation: 10,
-            zIndex: 3,
-            gridPosXColumn: '4',
-            gridPosYRow: '3',
-            relPosX: '-40px',
-            relPosY: '80px',
-        },
-    },
-    {
-        src: '/images/Spin-photo2.png',
-        aspectRatio: '0.91',
-        backCutOut: true,
-        backCutOutColor: 'sharpTeal',
-        imageNameAltText: 'ping pong area',
-        opacity: 1,
-        scale: 1.4, // scale transform for aceImage
-        rotation: -9, // rotation
-        zIndex: 1, // zindex for image container
-        gridPosXColumn: '4', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-        gridPosYRow: '1', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-        relPosX: '-40px', //relative position to grid position
-        relPosY: '80px',
-        paralax: 3,
+		mobile: {
+			aspectRatio: '0.95',
+			opacity: 1,
+			scale: 1.4,
+			rotation: 10,
+			zIndex: 3,
+			gridPosXColumn: '4',
+			gridPosYRow: '3',
+			relPosX: '-40px',
+			relPosY: '80px',
+		},
+	},
+	{
+		src: '/images/Spin-photo2.png',
+		aspectRatio: '0.91',
+		backCutOut: true,
+		backCutOutColor: 'sharpTeal',
+		imageNameAltText: 'ping pong area',
+		opacity: 1,
+		scale: 1.4, // scale transform for aceImage
+		rotation: -9, // rotation
+		zIndex: 1, // zindex for image container
+		gridPosXColumn: '4', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
+		gridPosYRow: '1', //enter the grid ROW start position. ex: 5 (start at grid line 5)
+		relPosX: '-40px', //relative position to grid position
+		relPosY: '80px',
+		paralax: 3,
 
-        mobile: {
-            aspectRatio: '0.91',
-            opacity: 1,
-            scale: 1.4,
-            rotation: 4,
-            zIndex: 1,
-            gridPosXColumn: '3',
-            gridPosYRow: '2',
-            relPosX: '45px',
-            relPosY: '80px',
-        },
-    },
+		mobile: {
+			aspectRatio: '0.91',
+			opacity: 1,
+			scale: 1.4,
+			rotation: 4,
+			zIndex: 1,
+			gridPosXColumn: '3',
+			gridPosYRow: '2',
+			relPosX: '45px',
+			relPosY: '80px',
+		},
+	},
 
-    {
-        src: '/images/ace-sticker-1.png',
-        aspectRatio: '1/1',
-        backCutOut: false,
-        backCutOutColor: 'sharpTeal',
-        imageNameAltText: 'ping pong area',
-        opacity: 1,
-        scale: 1.3, // scale transform for aceImage
-        rotation: -9, // rotation
-        zIndex: 3, // zindex for image container
-        gridPosXColumn: '3', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
-        gridPosYRow: '1', //enter the grid ROW start position. ex: 5 (start at grid line 5)
-        relPosX: '0px', //relative position to grid position
-        relPosY: '200px',
-        paralax: 1,
+	{
+		src: '/images/ace-sticker-1.png',
+		aspectRatio: '1/1',
+		backCutOut: false,
+		backCutOutColor: 'sharpTeal',
+		imageNameAltText: 'ping pong area',
+		opacity: 1,
+		scale: 1.3, // scale transform for aceImage
+		rotation: -9, // rotation
+		zIndex: 3, // zindex for image container
+		gridPosXColumn: '3', //enter the grid COLOMN start position. ex: 3 (start at grid line 3)
+		gridPosYRow: '1', //enter the grid ROW start position. ex: 5 (start at grid line 5)
+		relPosX: '0px', //relative position to grid position
+		relPosY: '200px',
+		paralax: 1,
 
-        mobile: {
-            aspectRatio: '1/1',
-            opacity: 1,
-            scale: 1.2,
-            rotation: 10,
-            zIndex: 1,
-            gridPosXColumn: '3',
-            gridPosYRow: '3',
-            relPosX: '-30px',
-            relPosY: '0px',
-        },
-    },
+		mobile: {
+			aspectRatio: '1/1',
+			opacity: 1,
+			scale: 1.2,
+			rotation: 10,
+			zIndex: 1,
+			gridPosXColumn: '3',
+			gridPosYRow: '3',
+			relPosX: '-30px',
+			relPosY: '0px',
+		},
+	},
 ];
