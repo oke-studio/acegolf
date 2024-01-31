5546025acdcf802fcd11a5d718ba975c120851eb
Seyitan Oke <seyitanoke@gmail.com>
2024-01-31 01:43:13 -0500

    started how it works page

--- src/components/howItWorks/howItWorks.component.tsx
+++ src/components/howItWorks/howItWorks.component.tsx
@@ -6,6 +6,8 @@ import { Typography } from '@/components/Typography/typography.component';
 import { Section } from '@/components/layout/section.component';
 import { HowItWorksInfoBox } from './components/howItWorksInfo/howItWorksInfo.component';
 import { TypeHowItWorksFields, TypeFaqItemFields } from '@/types/contentful';
+import { howItWorksImages } from './howItWorksImages';
+import { SectionImageGrid } from '../ImageLayoutGrids/sectionImageGrid.component';
 
 interface HowItWorksInfoBoxProps {
 	title: string;
@@ -79,96 +81,10 @@ export const HowItWorks = ({
 					{/* #1 */}
 				</Box>
 			</Box>
+			<SectionImageGrid
+				SectionImageGridWidth="rightHalf"
+				ImageData={howItWorksImages}
+			></SectionImageGrid>
 		</Section>
 	);
 };
-
-// <HowItWorksInfoBox
-//             number="1"
-//             label={
-//               <>
-//                 Reserve A <br />
-//                 Bay
-//               </>
-//             }
-//             description={
-//               <>
-//                 Whether you&apos;re a planner or procrastinator, we&apos;ve got
-//                 options for you. Make a reservation in advance with our super
-//                 simple online booking tool or just walk in and set up your tee
-//                 time.
-//               </>
-//             }
-//             miniInfoBoxOne={{
-//               question:
-//                 'This will be a quick FAQ here. See pricing and Schedume',
-//               answer:
-//                 'This will be a quick FAQ here. See pricing  and Schedume',
-//             }}
-//             miniInfoBoxTwo={{
-//               question:
-//                 'This will be a quick FAQ here. See pricing and Schedume',
-//               answer:
-//                 'This will be a quick FAQ here. See pricing  and Schedume',
-//             }}
-//           />
-
-//           <HowItWorksInfoBox
-//             number="2"
-//             label={
-//               <>
-//                 Set Up And
-//                 <br /> Tee Up
-//               </>
-//             }
-//             description={
-//               <>
-//                 Whether you&apos;re a planner or procrastinator, we&apos;ve got
-//                 options for you. Make a reservation in advance with our super
-//                 simple online booking tool or just walk in and set up your tee
-//                 time.
-//               </>
-//             }
-//             miniInfoBoxOne={{
-//               question:
-//                 'This will be a quick FAQ here. See pricing and Schedume',
-//               answer:
-//                 'This will be a quick FAQ here. See pricing  and Schedume',
-//             }}
-//             miniInfoBoxTwo={{
-//               question:
-//                 'This will be a quick FAQ here. See pricing and Schedume',
-//               answer:
-//                 'This will be a quick FAQ here. See pricing  and Schedume',
-//             }}
-//           />
-
-//           <HowItWorksInfoBox
-//             number="3"
-//             label={
-//               <>
-//                 Start <br />
-//                 Swingin&apos;
-//               </>
-//             }
-//             description={
-//               <>
-//                 Whether you&apos;re a planner or procrastinator, we&apos;ve got
-//                 options for you. Make a reservation in advance with our super
-//                 simple online booking tool or just walk in and set up your tee
-//                 time.
-//               </>
-//             }
-//             miniInfoBoxOne={{
-//               question:
-//                 'This will be a quick FAQ here. See pricing and Schedume',
-//               answer:
-//                 'This will be a quick FAQ here. See pricing  and Schedume',
-//             }}
-//             miniInfoBoxTwo={{
-//               question:
-//                 'This will be a quick FAQ here. See pricing and Schedume',
-//               answer:
-//                 'This will be a quick FAQ here. See pricing  and Schedume',
-//             }}
-//           />
