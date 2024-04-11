e15a63b5dd55b0b253808883b3c6b073b54590c8
Seyitan Oke <seyitanoke@gmail.com>
2024-04-11 09:03:15 -0400

    mocked button

--- src/pages/FAQ/components/FAQSection/FAQSection.tsx
+++ src/pages/FAQ/components/FAQSection/FAQSection.tsx
@@ -6,6 +6,8 @@ import { Spinner } from '../../../../components/LoadingStates/Spinner'
 import { motion } from 'framer-motion'
 import { FiChevronDown } from 'react-icons/fi'
 import { useGetFAQ } from '../../../../hooks/useGetFAQ/useGetFAQ.hook'
+import { Button } from '../../../../components/Button/Button'
+import { Link } from 'react-router-dom'
 
 export const FAQSection = () => {
   const { faqData, isLoading, isError } = useGetFAQ()
@@ -120,7 +122,7 @@ const FAQAccordion = ({
   return (
     <motion.div
       animate={open ? 'open' : 'closed'}
-      className="border-bold flex max-h-max w-full flex-col items-center gap-3 overflow-hidden rounded-2xl border-2 border-black bg-sharpTeal px-6 py-4 uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_orange] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none"
+      className="border-bold flex max-h-max w-full flex-col gap-3 overflow-hidden rounded-2xl border-2 border-black bg-sharpTeal px-6 py-4 text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_orange] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none"
       onClick={() => {
         // console.log('clicked', id)
         setOpen((pv) => !pv)
@@ -155,9 +157,16 @@ const FAQAccordion = ({
           height: open ? 'fit-content' : '0px',
           marginBottom: open ? '24px' : '0px',
         }}
-        className="black overflow-hidden"
+        className="black overflow-hidden flex gap-3 flex-col"
+       
       >
         {children}
+        <Button
+              buttonVariant="primary"
+              buttonStyle={{ width: 'max-content' }}
+            >
+              <Link to={"link"}> LinkText &rarr;</Link>
+            </Button>
       </motion.div>
     </motion.div>
   )
