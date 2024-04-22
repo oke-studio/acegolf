d5d5fc770d7079748fb9991f2d503d77c3c6288a
efe osah <onanefeosah@icloud.com>
2024-04-22 12:03:24 -0400

    lint errors

--- src/pages/Landing/components/WTVSection/WTVSection.tsx
+++ src/pages/Landing/components/WTVSection/WTVSection.tsx
@@ -2,13 +2,13 @@ import { Section } from '../../../../components/Section/Section'
 import { Container } from '../../../../components/Container/Container'
 
 // Images
-import URL1 from '../../../../assets/wtv/20231224_124126.mp4'
+// import URL1 from '../../../../assets/wtv/20231224_124126.mp4'
 import URL2 from '../../../../assets/wtv/20231224_124126.mp4'
 import URL3 from '../../../../assets/wtv/ace-pink.webp'
 import URL4 from '../../../../assets/wtv/ace_landing_wtv_demo.mp4' //video
 import URL5 from '../../../../assets/wtv/ace_landing_wtv_demo2.webm' //video
-import URL6 from '../../../../assets/wtv/wtv-01.webp'
-import URL7 from '../../../../assets/wtv/wtv-02.webp'
+// import URL6 from '../../../../assets/wtv/wtv-01.webp'
+// import URL7 from '../../../../assets/wtv/wtv-02.webp'
 import URL8 from '../../../../assets/wtv/wtv-03.webp'
 import URL9 from '../../../../assets/wtv/wtv-04.webp'
 import { Typography } from '../../../../components/Typography/Typography'
