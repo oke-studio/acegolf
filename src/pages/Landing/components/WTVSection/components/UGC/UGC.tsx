60e4731645dbc762213e2f082161e80a72f4c9f9
Seyitan Oke <seyitanoke@gmail.com>
2024-05-02 15:16:10 -0400

    removed local wtv assets for performace

--- src/pages/Landing/components/WTVSection/components/UGC/UGC.tsx
+++ src/pages/Landing/components/WTVSection/components/UGC/UGC.tsx
@@ -1,13 +1,3 @@
-// Images
-// import URL1 from '../../../../assets/wtv/20231224_124126.mp4'
-// import URL2 from '../../../../assets/wtv/20231224_124126.mp4'
-// import URL3 from '../../../../assets/wtv/ace-pink.webp'
-// import URL4 from '../../../../assets/wtv/ace_landing_wtv_demo.mp4' //video
-// import URL5 from '../../../../assets/wtv/ace_landing_wtv_demo2.webm' //video
-// // import URL6 from '../../../../assets/wtv/wtv-01.webp'
-// // import URL7 from '../../../../assets/wtv/wtv-02.webp'
-// import URL8 from '../../../../assets/wtv/wtv-03.webp'
-// import URL9 from '../../../../assets/wtv/wtv-04.webp'
 import { ImageURLFormatter } from '../../../../../../utils/imageFormatter'
 import { useGetUGCTestimonials } from './hooks/useGetUGC.hook'
 
