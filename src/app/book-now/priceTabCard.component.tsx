9481980d12d52467e4987c930f17d8d4348809a2
Seyitan Oke <seyitanoke@gmail.com>
2024-05-10 18:21:08 +0530

    fixed the mismatch because contenful had monday as value 0, while js has sunday as value 0. Added a -1 to align both

--- src/app/book-now/priceTabCard.component.tsx
+++ src/app/book-now/priceTabCard.component.tsx
@@ -352,7 +352,11 @@ export default function PriceTabCard() {
 
 	const currentDate = new Date();
 
+	console.log(currentDate);
+
 	const [value, setValue] = React.useState(currentDate.getDay());
+	console.log(value);
+	console.log(value-1);
 
 	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
 		setValue(newValue);
@@ -384,19 +388,19 @@ export default function PriceTabCard() {
 					scrollbarWidth: 'none',
 				}}
 			>
-				<StyledTab value={0} label="Mon" />
-				<StyledTab value={1} label="Tue" />
-				<StyledTab value={2} label="Wed" />
-				<StyledTab value={3} label="Thu" />
-				<StyledTab value={4} label="Fri" />
-				<StyledTab value={5} label="Sat" />
-				<StyledTab value={6} label="Sun" />
+				<StyledTab value={1} label="Mon" />
+				<StyledTab value={2} label="Tue" />
+				<StyledTab value={3} label="Wed" />
+				<StyledTab value={4} label="Thu" />
+				<StyledTab value={5} label="Fri" />
+				<StyledTab value={6} label="Sat" />
+				<StyledTab value={0} label="Sun" />
 			</StyledTabs>
 
 			{baysPricingReduced &&
 				baysPricingReduced.map((bay, index) => (
 					<TabPanel
-						value={value}
+						value={value - 1}
 						index={index}
 						dir={theme.direction}
 						key={`bay_${index}_${bay.day}`}
