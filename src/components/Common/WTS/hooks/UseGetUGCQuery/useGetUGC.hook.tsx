ba8f5d16fc3bf594cc6ef6e4d109cc45b4272e2d
efe osah <onanefeosah@icloud.com>
2024-05-10 15:34:53 -0400

    lint fix_02

--- src/components/Common/WTS/hooks/UseGetUGCQuery/useGetUGC.hook.tsx
+++ src/components/Common/WTS/hooks/UseGetUGCQuery/useGetUGC.hook.tsx
@@ -1,28 +1,28 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAceQuery } from '../../../../../utils/getAceQuery'
-import { GetUGCTestimonialsQuery } from '../../../../../utils/Queries'
+// import { useQuery } from '@tanstack/react-query'
+// import { getAceQuery } from '../../../../../utils/getAceQuery'
+// import { GetUGCTestimonialsQuery } from '../../../../../utils/Queries'
 import { useGetAceQuery } from '../../../../../hooks/UseGetAceQuery/useGetAceQuery.hook'
 
-function useGetAceUGCTestimonialsQuery() {
-  const Query = useQuery<{
-    data: {
-      testimonialsCollection: {
-        items: {
-          nameOfPerson: string
-          testimonialMessage: string
-          type: 'UGC Video' | 'Testimonial'
-          linkedUrl?: string
-          imageVideo: { contentType: string; url: string; fileName: string }
-        }[]
-      }
-    }
-  }>({
-    queryKey: ['ace_testimonials_v2'],
-    queryFn: () => getAceQuery(GetUGCTestimonialsQuery),
-  })
+// function useGetAceUGCTestimonialsQuery() {
+//   const Query = useQuery<{
+//     data: {
+//       testimonialsCollection: {
+//         items: {
+//           nameOfPerson: string
+//           testimonialMessage: string
+//           type: 'UGC Video' | 'Testimonial'
+//           linkedUrl?: string
+//           imageVideo: { contentType: string; url: string; fileName: string }
+//         }[]
+//       }
+//     }
+//   }>({
+//     queryKey: ['ace_testimonials_v2'],
+//     queryFn: () => getAceQuery(GetUGCTestimonialsQuery),
+//   })
 
-  return Query
-}
+//   return Query
+// }
 
 export function useGetUGCTestimonials() {
   //   const { data, isLoading, isError } = useGetAceUGCTestimonialsQuery()
