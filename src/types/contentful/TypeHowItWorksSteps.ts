2579bb911e23b3e5dc568b872d4b9364cf9aea74
efe osah <onanefeosah@icloud.com>
2024-07-13 15:43:20 -0400

    removing everything from main

--- src/types/contentful/TypeHowItWorksSteps.ts
+++
@@ -1,9 +0,0 @@
-import type { Entry, EntryFields } from 'contentful';
-
-export interface TypeHowItWorksStepsFields {
-	title?: string;
-	description?: string;
-	relatedFaq?: { question: string; answer: string };
-}
-
-// export type TypeHowItWorksSteps = Entry<TypeHowItWorksStepsFields>;
--- src/types/contentful/TypeServiceTime.ts
+++
@@ -1,9 +0,0 @@
-import type { Entry, EntryFields } from 'contentful';
-
-export interface TypeServiceTimeFields {
-	nameOfServiceTime: 'Full Service - Late' | 'Full Service' | 'Partial Service';
-	beginningTime: string;
-	endTime: string;
-}
-
-// export type TypeServiceTime = Entry<TypeServiceTimeFields>;
